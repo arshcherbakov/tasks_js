3d6556a6b30aee34073f84ca932d63bdeac2c9c9
arshcherbakov <shcherbakov.fortech@gmail.com>
2024-01-25 10:16:03 +0300

    fix task12

--- task12.js
+++ task12.js
@@ -1,34 +1,32 @@
-
-/*
-12. Переставить местами максимальный и минимальный элемент в массиве
+/* 
+15. Заполните массив следующим образом: в первый элемент запишите 'x', во второй 'xx', в третий 'xxx' и так далее. 
+Функция параметром принимает длину массива.
 Input:
-[5, 7, 2, 9, 5, 6, 3, 1, 8]
+7
+
 Output:
-[5, 7, 2, 1, 5, 6, 3, 9, 8]
+[
+  x,
+  xx,
+  xxx,
+  xxxx,
+  xxxxx,
+  xxxxxx,
+  xxxxxxx
+]
 
 */
 
-// Solution 1
-const removeMaxMinElement = (array) => {
-  const maxNumber = Math.max(...array);
-  const minNumber = Math.min(...array);
-  const indexMaxNumber = array.indexOf(maxNumber);
-  const indexMinNumber = array.indexOf(minNumber);
-  array[indexMaxNumber] = minNumber;
-  array[indexMinNumber] = maxNumber;
+const fillingArray = (lengthArray) => {
+
+  const array = [];
+  let char = '';
+
+  for (let i = 0; i < lengthArray; i++) {
+    array.push(char += 'x');
+  }
+  
   return array;
 }
 
-// Solution 2 если не важно последовательность других елемнт. в массиве 
-
-const removeMaxMinElement = (array) => {
-  array.sort((a, b) => a - b);
-  const max = array[array.length - 1];
-  const min = array[0];
-  array[array.length - 1] = min;
-  array[0] = max;
-  return array;
-}
-
-
-console.log(removeMaxMinElement([5, 7, 2, 9, 5, 6, 3, 1, 8]));
+console.log(fillingArray(7));
