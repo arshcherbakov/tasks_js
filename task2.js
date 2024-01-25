3a4556a6e97ebf209d7a0d0225a267b1358b9615
arshcherbakov <shcherbakov.fortech@gmail.com>
2024-01-25 09:51:43 +0300

    fix task2

--- task2.js
+++ task2.js
@@ -11,20 +11,21 @@ Output:
 
 */
 
-const inputCharString = (str) => {
+const outputCharString = (str) => {
+	
 	const length_str = str.length;
+	let new_str = '';
 
 	if (length_str > 5) {
-		return str[0] + str[1] + str[2] + str[length_str - 3] + str[length_str - 2] + str[length_str - 1];
-	} else {
-		let new_str = '';
-		for (let i = 0; i < length_str; i++) {
-			new_str += str[0];
-		}
-		return new_str;
+		return str.slice(0, 3) + str.slice(-3);
 	}
+
+	for (let i = 0; i < length_str; i++) {
+		new_str += str[0];
+	}
+
+	return new_str;
 }
 
-console.log(inputCharString('test education part 2'));
-
-console.log(inputCharString('text'));
+console.log(outputCharString('test education part 2'));
+console.log(outputCharString('text'));
